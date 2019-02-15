f496063f89374d1c962fb9f2af77bb1521cfd6af
Br1 <a01700879@itesm.mx>
2019-02-15 13:06:17 -0600

    organizar archivos

---
+++ distribucion/dis_pi.cpp
@@ -0,0 +1,53 @@
+/*------------------------------------------------------------------------
+*
+* Metodos Cuatitativos: Metodo Directo para generar variables aleatorias
+* Fecha: 15-Feb-2019
+* Autor: A00569364 Eduardo Larios Fernández
+* Procesamiento Paralelo con OpenMP
+*
+*-------------------------------------------------------------------------*/
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "header.h"
+
+int main()
+{
+  /* # of points in the 1st quadrant the circle */
+  time_t t;
+  int i;
+  double ms = 0;
+  long long int tosses, hits = 0;
+  double x, y;
+  double z, pi;
+
+  printf("Ingrese el numero de variables para estimar PI: ");
+  scanf("%lld", &tosses);
+
+  /* initialize random numbers */
+  srand((unsigned)time(&t));
+
+  printf("Starting...\n");
+  start_timer();
+
+  #pragma omp parallel for shared(hits) private(i, x, y, z)
+  for (i = 0; i < tosses; i++)
+  {
+    x = (double)rand() / RAND_MAX;
+    y = (double)rand() / RAND_MAX;
+    z = x * x + y * y;
+    if (z <= 1) hits++;
+  }
+
+  ms = stop_timer();
+
+  pi = (double)hits / tosses * 4;
+  printf("Numero de variables: %lld\nPi value: %g \n", tosses, pi);
+  printf("Tiempo tomado: %.5lf ms\n", ms);
+
+  return 0;
+
+}
